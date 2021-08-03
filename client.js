590c075533d51b69b189e218e14eef97da742847
Michael <michaelladouceur1@gmail.com>
2021-08-03 16:19:02 -0500

    exploration of various data types, passing arguments, graphql api calls using axios and fetch, and client variables in queries

--- client.js
+++ client.js
@@ -1,11 +1,43 @@
 const axios = require("axios");
 
-async function getData() {
-  console.log("GetData");
-  try {
-    const { data } = await axios.post("/graphql", { query: "{hello}" });
-    return data;
-  } catch (error) {
-    return error;
-  }
-}
+// async function getData() {
+//   console.log("GetData");
+//   try {
+//     const { data } = await axios.post("/graphql", { query: "{hello}" });
+//     return data;
+//   } catch (error) {
+//     return error;
+//   }
+// }
+
+// -----------------Client for Schema & Root 3
+const fetch = require("node-fetch");
+
+var dice = 10;
+var sides = 6;
+var query = `query RollDice($dice: Int!, $sides: Int) {
+  rollDice(numDice: $dice, numSides: $sides)
+}`;
+
+// fetch("http://localhost:4000/graphql", {
+//   method: "POST",
+//   headers: {
+//     "Content-Type": "application/json",
+//     Accept: "application/json",
+//   },
+//   body: JSON.stringify({
+//     query,
+//     variables: { dice, sides },
+//   }),
+// })
+//   .then((r) => r.json())
+//   .then((data) => console.log("Data returned with Fetch:", data));
+
+axios
+  .post("http://localhost:4000/graphql", {
+    query: query,
+    variables: { dice, sides },
+  })
+  .then(({ data }) => {
+    console.log("Data returned with Axios: ", data);
+  });
--- server.js
+++ server.js
@@ -2,15 +2,54 @@ const express = require("express");
 const { graphqlHTTP } = require("express-graphql");
 const { buildSchema } = require("graphql");
 
+// ------------------------------Schema & Root 1
+// const schema = buildSchema(`
+//     type Query {
+//         hello: String
+//     }
+// `);
+
+// const root = {
+//   hello: () => {
+//     return "Hello world";
+//   },
+// };
+
+// -----------------------------Schema & Root 2
+// const schema = buildSchema(`
+//     type Query {
+//         quoteOfTheDay: String
+//         random: Float!
+//         rollThreeDice: [Int]
+//     }
+// `);
+
+// const root = {
+//   quoteOfTheDay: () => {
+//     return Math.random() < 0.5 ? "Take it easy" : "Salvation lies within";
+//   },
+//   random: () => {
+//     return Math.random();
+//   },
+//   rollThreeDice: () => {
+//     return [1, 2, 3].map((_) => 1 + Math.floor(Math.random() * 6));
+//   },
+// };
+
+// -----------------------------Schema & Root 3
 const schema = buildSchema(`
     type Query {
-        hello: String 
+        rollDice(numDice: Int!, numSides: Int): [Int]
     }
 `);
 
 const root = {
-  hello: () => {
-    return "Hello world";
+  rollDice: ({ numDice, numSides }) => {
+    let output = [];
+    for (let i = 0; i < numDice; i++) {
+      output.push(1 + Math.floor(Math.random() * (numSides || 6)));
+    }
+    return output;
   },
 };
 
